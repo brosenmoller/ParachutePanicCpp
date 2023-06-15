ccc7334db72062986508f4c73532c81d8704d363
brosenmoller <rosenmollerben@gmail.com>
2023-06-15 22:51:32 +0200

    Moving Enemies

--- src/GameObjects/Enemy.cpp
+++ src/GameObjects/Enemy.cpp
@@ -2,20 +2,43 @@
 #include "../SceneManager.hpp"
 #include "../LogManager.hpp"
 
+bool movingRight;
+sf::Clock turningClock;
+float delay;
+
 void Enemy::Start()
 {
-	speed = 400 + (rand() % 100);
+	speed = 400 + (std::rand() % 100);
 	mass = 50;
 	collisionRange = 45;
+
+	movingRight = std::rand() % 2;
+	delay = std::rand() % 2000;
 }
 
 void Enemy::Update()
 {
 	UpdatePhysics();
 
-	velocity = Vector2::Down() * speed;
-	//velocity = Vector2(velocity.x, speed);
-	//SetForce(Vector2(-40000 + (rand() % 80000), 0));
+	//velocity = Vector2::Down() * speed;
+
+	if (turningClock.getElapsedTime().asMilliseconds() > delay)
+	{
+		delay = std::rand() % 2000;
+		movingRight = std::rand() % 2;
+		turningClock.restart();
+	}
+
+	velocity = Vector2(velocity.x, speed);
+	if (movingRight)
+	{
+		SetForce(Vector2(30000, 0));
+	}
+	else
+	{
+		SetForce(Vector2(-30000, 0));
+	}
+	
 
 	float distanceToPlayer = position.Distance(sceneManager->player->GetPosition());
 	if (distanceToPlayer < collisionRange)
@@ -26,5 +49,14 @@ void Enemy::Update()
 	if (position.y > SCREEN_HEIGHT)
 	{
 		sceneManager->DestroyGameObject(this);
+	}
+
+	if (position.x > SCREEN_WIDTH - 120)
+	{
+		position.x = SCREEN_WIDTH - 120;
+	}
+	else if (position.x < 70)
+	{
+		position.x = 70;
 	}
 }
